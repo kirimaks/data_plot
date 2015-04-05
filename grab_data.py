30a68776ce00527626a3bbb11daaba72c73f4820
kirimaks <kirimaks@yahoo.com>
2015-04-05 17:14:28 +0300

    reorganise code

--- grab_data.py
+++ grab_data.py
@@ -1,98 +1,91 @@
-import time
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# System files.
-cpu_temp_file = open('/sys/class/hwmon/hwmon0/temp1_input')
-load_avg_file = open('/proc/loadavg')
-
-# Working directory.
-work_dir = '/tmp/gnuplot/'
-
-# Data files.
-cpu_temp_out_file = work_dir + 'temp_data.txt'
-load_avg_out_file = work_dir + 'avg_data.txt'
-
-global_delay = 2
-max_data_entries = 2500 
-
-def truncate_file(file_to_truncate):
-    lines_in_file = sum(1 for l in open(load_avg_out_file))
-
-    if lines_in_file < max_data_entries:
-        return
-
-    fl = open(file_to_truncate)
-    cur_lines = fl.readlines()
-    fl.close()
-
-    fl = open(file_to_truncate, 'w')
-    for line in cur_lines[1:-1]:
-        fl.write(line)
-
-    fl.close()
+import config
+import os.path
+import sys
 
 
-def get_cur_traffic(iface):
-    rx_file = '/sys/class/net/' + iface + '/statistics/rx_bytes'
-    tx_file = '/sys/class/net/' + iface + '/statistics/tx_bytes'
-    traffic_out_file = work_dir + 'traff_' + iface + '.txt'
-
-    r1 = open(rx_file).readline()
-    t1 = open(tx_file).readline()
-    time.sleep(1)
-    r2 = open(rx_file).readline()
-    t2 = open(tx_file).readline()
-
-    cur_rx = str((int(r2)-int(r1))/1024)
-    cur_tx = str((int(t2)-int(t1))/1024)
-
-    traffic_out = open(traffic_out_file, 'a')
-    traffic_out.write(cur_rx + '\t')
-    traffic_out.write(cur_tx + '\n')
-    traffic_out.close()
-
-    truncate_file(traffic_out_file)
+tasks = [ config.cpu_temp, config.load_average, config.network_statistic ]
 
 
-while True:
-    ########################
-    #### Cpu temperature ###
-    ########################
-    cpu_temp_out = open(cpu_temp_out_file, 'a')
-    cur_temp = cpu_temp_file.read()[:2]     # Read current temperature.
-    cpu_temp_file.seek(0)                   # Rewind file.
-    cpu_temp_out.write(cur_temp + '\n')     # Write current temperature.
-    cpu_temp_out.close()
+def regular_file(file_data):
+    if __debug__:
+        print u'\t<<%s>>(regular file)' % file_data[u'in_file']
 
-    #lines_in_file = sum(1 for l in open(cpu_temp_out_file))
-    #if lines_in_file >= max_data_entries:   
-    #    truncate_file(cpu_temp_out_file)
-    truncate_file(cpu_temp_out_file)
-    
-    ####################
-    ### Load average ###
-    ####################
-    load_avg_out = open(load_avg_out_file, 'a')
-    cur_avg = load_avg_file.read()
-    load_avg_file.seek(0)
-    load_avg_out.write(cur_avg[:14] + '\n')
-    load_avg_out.close() # Flush.
-
-    #lines_in_file = sum(1 for l in open(load_avg_out_file))
-    #if lines_in_file >= max_data_entries:
-    #    truncate_file(load_avg_out_file)
-    truncate_file(load_avg_out_file)
-
-    ########################
-    ### Cpu usage by top ###
-    ########################
-
-    ########################
-    ### Traffic (mbytes) ###
-    ########################
-
-    #get_cur_traffic('eth0')
-    get_cur_traffic('wlp1s0')
+    cur_path = os.path.join(file_data[u'path'], file_data[u'in_file'])
+    cur_line = None
 
 
-    ####
-    time.sleep(global_delay)  # Global delay.
+    # Reading.
+    with open(cur_path) as cur_file:
+        cur_line = cur_file.readline()
+        if __debug__:
+            print u'\t\t%s' %  cur_line
+
+
+    # Prepare line for different tasks.
+    if file_data[u'title'] == u'cpu_temp':
+        cur_line = list(cur_line)
+        cur_line.insert(2, u'.')
+        cur_line = ''.join(cur_line)
+
+
+    # Writing.
+    cur_path = os.path.join(config.work_dir, file_data['out_file'])
+    with open(cur_path, 'a') as out_file:
+        out_file.write(cur_line)
+
+
+
+# Return number of lines in a file.
+def file_len(f):
+    num_lines = 0
+
+    with open(f) as fl:
+        for line in fl:
+            num_lines += 1
+
+    if __debug__:
+        print '\t\t%s : %d lines.' % (f, num_lines)
+
+    return num_lines
+
+
+
+def truncate_file(f):
+    if file_len(f) >= config.max_lines_in_file: 
+        if __debug__:
+            print '\t\tTruncate file: %s' % f
+
+        # OS specific code.
+        if sys.platform == 'linux2':
+            os.system('sed -i -e "1d" ' + f)
+            
+
+
+
+# Check work directory and create if need.
+if not os.path.isdir(config.work_dir):
+    if __debug__:
+        print 'Create direcotry: %s' % config.work_dir
+    os.mkdir(config.work_dir)
+
+
+
+
+################
+# Main program #
+################
+
+for task in tasks:
+    if __debug__:
+        print '*** %s ***' % task[u'title']
+
+    if task[u'title'] == 'network_statistic':
+        for iface in  task[u'ifaces']:
+            print '\t[%s]' % iface
+    else:
+        regular_file(task)
+        # Use truncate only for regular temporary.
+        truncate_file(os.path.join(config.work_dir, task[u'out_file']))
